ae62f3d990a2fe18e2eb9b6554a467dac597b052
Lorenzo Scocco <lorenzoscocco70@gmail.com>
2026-04-04 21:07:56 +0200

    Initialized server with express and modules for the backend

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.static('../frontend'))
+
+/*app.post('/', (req, res) => {
+  console.log(req.body)
+  res.json({result: true})
+})*/
+
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000...')
+})
