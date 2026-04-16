d1b663e7f5d8c27b367bdaccc866485f76315225
Lorenzo Scocco <lorenzoscocco70@gmail.com>
2026-04-16 12:55:01 +0200

    Implemented full submit movement and starting to implement monthly report

--- frontend/javascript/preview-functions.js
+++ frontend/javascript/preview-functions.js
@@ -56,6 +56,8 @@ export function initializeSubmitMovement(data, container) {
 
       const completedRes = await res.json()
       console.log(completedRes) // success-not success
+      data.splice(index, 1)
+      renderMovements(data, container);
 
     } catch(err) {
       console.log(err)
